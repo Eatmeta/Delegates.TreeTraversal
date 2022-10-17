5181c44181bc5bcd4c4bb5dcb0aef0e617688a03
Eatmeta <metagroul@gmail.com>
2022-10-17 14:33:57 +0300

    Initial commit

---
+++ Traversal.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Delegates.TreeTraversal
+{
+    public static class Traversal
+    {
+        public static IEnumerable<Product> GetProducts(ProductCategory root)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Job> GetEndJobs(Job root)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
