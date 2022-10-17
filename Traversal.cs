e2c3d67bb05d831e0f7220abb94d3d8e121d2a49
Eatmeta <metagroul@gmail.com>
2022-10-17 14:48:07 +0300

    Implement universal method DoTraversal that traverse different types of trees

--- Traversal.cs
+++ Traversal.cs
@@ -5,19 +5,53 @@ namespace Delegates.TreeTraversal
 {
     public static class Traversal
     {
-        public static IEnumerable<Product> GetProducts(ProductCategory root)
+        private static List<TValue> DoTraversal<TTree, TValue>(
+            Func<TTree, IEnumerable<TTree>> enumerable,
+            Func<TTree, bool> selector,
+            Func<TTree, IEnumerable<TValue>> initial,
+            List<TValue> result,
+            TTree root)
         {
-            throw new NotImplementedException();
+            if (root == null)
+                return new List<TValue>();
+
+            if (selector(root))
+                result.AddRange(initial(root));
+
+            foreach (var tree in enumerable(root))
+                DoTraversal(enumerable, selector, initial, result, tree);
+
+            return result;
         }
 
-        public static IEnumerable<Job> GetEndJobs(Job root)
+        public static IEnumerable<Product> GetProducts(ProductCategory root)
         {
-            throw new NotImplementedException();
+            return DoTraversal(
+                x => x.Categories,
+                x => true,
+                x => x.Products,
+                new List<Product>(),
+                root);
         }
 
         public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> root)
         {
-            throw new NotImplementedException();
+            return DoTraversal(
+                x => new List<BinaryTree<T>> {x.Left, x.Right},
+                x => x.Left == null && x.Right == null,
+                x => new List<T> {x.Value},
+                new List<T>(),
+                root);
+        }
+
+        public static IEnumerable<Job> GetEndJobs(Job root)
+        {
+            return DoTraversal(
+                x => x.Subjobs,
+                x => x.Subjobs.Count == 0,
+                x => new List<Job> {x},
+                new List<Job>(),
+                root);
         }
     }
 }
